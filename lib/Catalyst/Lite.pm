018e532f4ab60bbbd57b29e81a24324ee02eaab4
Eden Cardim <edencardim@gmail.com>
2011-06-15 04:40:29 -0300

    added script runner and docs

--- lib/Catalyst/Lite.pm
+++ lib/Catalyst/Lite.pm
@@ -1,4 +1,22 @@
 package Catalyst::Lite;
+
+=head1 NAME
+
+Catalyst::Lite - The elegant MVC Web Application Framework, on a veggie diet
+
+=head1 SYNOPSIS
+
+package MyApp;
+use Catalyst::Lite;
+
+get '/' => sub { pop->res->body('hello world!') };
+
+MyApp->setup
+
+# perl lib/MyApp.pm server
+
+=cut
+
 require Catalyst;
 use Moose;
 use Moose::Exporter;
@@ -20,6 +38,19 @@ sub import {
     setup_dispatcher => sub {
       my ($class) = @_;
       register_lite_actions($class);
+    }
+  );
+
+  $app_meta->add_after_method_modifier(
+    setup_finalize => sub {
+      my ($class) = @_;
+      if ( $ARGV[0] eq 'server' ) {
+        shift @ARGV;
+        Class::MOP::load_class('Catalyst::Script::Server');
+        $class->setup_engine('Catalyst::Engine::HTTP');
+        Catalyst::Script::Server->new_with_options( application_name => $class )
+          ->run;
+      }
     }
   );
 
@@ -58,14 +89,14 @@ sub register_lite_actions {
   my $dispatcher = $class->dispatcher;
 
   foreach my $action (@lite_registrations) {
-    my %attrs = @$action;
-    my $role = delete $attrs{role};
-    my $role_args = $role->{args};
-    my $role_class  = 'Catalyst::Lite::DispatchType::Role::' . $role->{name};
+    my %attrs      = @$action;
+    my $role       = delete $attrs{role};
+    my $role_args  = $role->{args};
+    my $role_class = 'Catalyst::Lite::DispatchType::Role::' . $role->{name};
     Class::MOP::load_class($role_class);
     my $action = $class->create_action( %attrs, class => $class );
     Moose::Util::apply_all_roles( $action, $role_class );
-    $action->$_($role_args->{$_}) for keys %$role_args;
+    $action->$_( $role_args->{$_} ) for keys %$role_args;
     $dispatcher->register( $class, $action );
   }
 }
